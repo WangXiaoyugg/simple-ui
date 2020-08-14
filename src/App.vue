f95d053a3db9456c98185fad35cbac8ef3d5be63
GarenWang <2270099149@qq.com>
2020-08-14 17:37:00 +0800

    初始化项目

---
+++ src/App.vue
@@ -0,0 +1,15 @@
+<template>
+  <div>HI</div>
+  <Garen />
+</template>
+
+<script>
+import Garen from "./components/Garen.vue";
+
+export default {
+  name: "App",
+  components: {
+    Garen
+  }
+};
+</script>
