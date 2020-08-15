f196a8eef2eec7374f97b19d1d4d1a4e9da0744d
GarenWang <2270099149@qq.com>
2020-08-15 18:52:52 +0800

    实现topnav响应式

--- src/components/Topnav.vue
+++ src/components/Topnav.vue
@@ -1,10 +1,25 @@
 <template>
   <div class="topnav">
-    <div class="logo" @click="toggleAside">Simple UI</div>
+    <div class="logo">Simple UI</div>
     <ul class="menu">
       <li>菜单1</li>
       <li>菜单2</li>
     </ul>
+    <span class="toggleAside" @click="toggleAside">
+      <svg
+        height="24"
+        class="octicon octicon-three-bars"
+        viewBox="0 0 16 16"
+        version="1.1"
+        width="24"
+        aria-hidden="true"
+      >
+        <path
+          fill-rule="evenodd"
+          d="M1 2.75A.75.75 0 011.75 2h12.5a.75.75 0 110 1.5H1.75A.75.75 0 011 2.75zm0 5A.75.75 0 011.75 7h12.5a.75.75 0 110 1.5H1.75A.75.75 0 011 7.75zM1.75 12a.75.75 0 100 1.5h12.5a.75.75 0 100-1.5H1.75z"
+        />
+      </svg>
+    </span>
   </div>
 </template>
 
@@ -31,6 +46,8 @@ export default {
   padding: 16px;
   position: relative;
   z-index: 10;
+  justify-content: center;
+  align-items: center;
   > .logo {
     max-width: 6em;
     margin-right: auto;
@@ -44,6 +61,29 @@ export default {
     > li {
       margin: 0 1em;
       color: #fff;
+    }
+  }
+  .toggleAside {
+    width: 24px;
+    height: 24px;
+    position: absolute;
+    left: 16px;
+    top: 50%;
+    transform: translateY(-50%);
+    display: none;
+    > svg {
+      fill: #eee;
+    }
+  }
+  @media (max-width: 500px) {
+    .menu {
+      display: none;
+    }
+    .logo {
+      margin: 0 auto;
+    }
+    .toggleAside {
+      display: inline-block;
     }
   }
 }
