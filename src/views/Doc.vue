a3f5944f003893e556776f5782ff51fe2947e72a
GarenWang <2270099149@qq.com>
2020-08-15 18:26:53 +0800

    初始化的aside的html

--- src/views/Doc.vue
+++ src/views/Doc.vue
@@ -2,7 +2,23 @@
   <div>
     <Topnav />
     <div class="content">
-      <aside>侧边栏</aside>
+      <aside>
+        <h2>组件列表</h2>
+        <ol>
+          <li>
+            <router-link to="/doc/switch">Switch组件</router-link>
+          </li>
+          <li>
+            <router-link to="/doc/button">Button组件</router-link>
+          </li>
+          <li>
+            <router-link to="/doc/dialog">Dialog组件</router-link>
+          </li>
+          <li>
+            <router-link to="/doc/tabs">Tabs组件</router-link>
+          </li>
+        </ol>
+      </aside>
       <main>主内容</main>
     </div>
   </div>
