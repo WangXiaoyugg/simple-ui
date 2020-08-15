565996315831505ceeec34ec5ea746d6c02276a4
GarenWang <2270099149@qq.com>
2020-08-15 18:27:57 +0800

    添加aside的样式

--- src/views/Doc.vue
+++ src/views/Doc.vue
@@ -35,4 +35,20 @@ export default {
 
 
 <style scoped lang="scss">
+aside {
+  background: skyblue;
+  width: 150px;
+  padding: 16px;
+  position: fixed;
+  top: 0;
+  left: 0;
+  > h2 {
+    margin-bottom: 4px;
+  }
+  > ol {
+    li {
+      padding: 4px 0;
+    }
+  }
+}
 </style>
