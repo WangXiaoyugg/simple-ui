5e8bf5310b036b421a79b043722ccd8752a9a943
GarenWang <2270099149@qq.com>
2020-08-16 22:46:03 +0800

    button支持事件

--- src/demos/ButtonDemo.vue
+++ src/demos/ButtonDemo.vue
@@ -1,3 +1,22 @@
 <template>
   <div>Button 示例</div>
+  <section>
+    <h2>示例1</h2>
+    <Button @click="onClick" @focus="onClick" @mouseover="onClick" size="large">你好</Button>
+  </section>
 </template>
+
+<script lang="ts">
+import Button from "../lib/Button.vue";
+export default {
+  components: {
+    Button
+  },
+  setup() {
+    const onClick = () => {
+      console.log("click");
+    };
+    return { onClick };
+  }
+};
+</script>
---
+++ src/lib/Button.vue
@@ -0,0 +1,17 @@
+<template>
+  <div :size="size">
+    <button v-bind="rest">
+      <slot />
+    </button>
+  </div>
+</template>
+
+<script lang="ts">
+export default {
+  inheritAttrs: false,
+  setup(props, context) {
+    const { size, ...rest } = context.attrs;
+    return { size, rest };
+  }
+};
+</script>
