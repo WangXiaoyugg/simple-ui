f9808b31f1af9d55b2c60c4f00024023706aaf8d
GarenWang <2270099149@qq.com>
2020-08-16 18:54:28 +0800

    删除测试代码

--- src/App.vue
+++ src/App.vue
@@ -3,14 +3,10 @@
 </template>
 
 <script>
-import Garen from "./components/Garen.vue";
 import { ref, provide } from "vue";
 
 export default {
   name: "App",
-  components: {
-    Garen
-  },
   setup() {
     const documentWidth = document.documentElement.clientWidth;
     const asideVisible = ref(documentWidth < 500 ? false : true);
