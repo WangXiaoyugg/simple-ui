cee65d5e8d2500760c170797a5efc63b69c3c477
GarenWang <2270099149@qq.com>
2020-08-16 21:16:17 +0800

    switch 切换开关状态

--- src/demos/SwitchDemo.vue
+++ src/demos/SwitchDemo.vue
@@ -1,3 +1,14 @@
 <template>
-  <div>Switch 示例</div>
+  <div>
+    <Switch />
+  </div>
 </template>
+
+<script lang="ts">
+import Switch from "../lib/Switch.vue";
+export default {
+  components: {
+    Switch
+  }
+};
+</script>
---
+++ src/lib/Switch.vue
@@ -0,0 +1,46 @@
+<template>
+  <button @click="toggle" :class="{checked}">
+    <span class></span>
+  </button>
+</template>
+
+<script lang="ts">
+import { ref } from "vue";
+export default {
+  setup() {
+    const checked = ref(false);
+    const toggle = () => {
+      checked.value = !checked.value;
+    };
+    return { checked, toggle };
+  }
+};
+</script>
+
+<style lang="scss" scoped>
+$h: 22px;
+$h2: $h - 4px;
+button {
+  height: $h;
+  width: $h * 2;
+  border: none;
+  background: #eee;
+  border-radius: $h / 2;
+  position: relative;
+}
+span {
+  position: absolute;
+  top: 2px;
+  left: 2px;
+  height: $h2;
+  width: $h2;
+  background: #fff;
+  border-radius: $h / 2;
+}
+button.checked {
+  background: skyblue;
+}
+button.checked > span {
+  left: calc(100% - #{$h2} - 2px);
+}
+</style>
