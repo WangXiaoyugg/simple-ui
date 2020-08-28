1c1c01063d806ada5710b4f91a6e19da5bd7e708
GarenWang <2270099149@qq.com>
2020-08-28 08:27:43 +0800

    创建Dialog组件

--- src/demos/DialogDemo.vue
+++ src/demos/DialogDemo.vue
@@ -1,3 +1,16 @@
 <template>
   <div>Dialog 示例</div>
+  <section>
+    <h2>实例1</h2>
+    <Dialog />
+  </section>
 </template>
+
+<script lang="ts">
+import Dialog from "../lib/Dialog.vue";
+export default {
+  components: {
+    Dialog
+  }
+};
+</script>
---
+++ src/lib/Dialog.vue
@@ -0,0 +1,23 @@
+<template>
+  <div class="overlay"></div>
+  <div class>
+    <header>这是标题</header>
+    <main>
+      <p>第一行文字</p>
+      <p>第二行文字</p>
+    </main>
+    <footer>
+      <Button>取消</Button>
+      <Button>确定</Button>
+    </footer>
+  </div>
+</template>
+
+<script lang="ts">
+import Button from "./Button.vue";
+export default {
+  components: {
+    Button
+  }
+};
+</script>
