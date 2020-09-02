c22f879bb18a1419a1a3227037a2a27307fe2b01
GarenWang <2270099149@qq.com>
2020-09-03 07:25:59 +0800

    开发Tabs和Tab组件

--- src/demos/TabsDemo.vue
+++ src/demos/TabsDemo.vue
@@ -1,3 +1,29 @@
 <template>
   <div>Tabs 示例</div>
+  <section>
+    <h1>示例1</h1>
+    <Tabs v-model:selected="x">
+      <Tab title="导航1">内容1</Tab>
+      <Tab title="导航2222">内容2</Tab>
+    </Tabs>
+  </section>
 </template>
+
+<script>
+import { ref } from "vue";
+import Tab from "../lib/Tab.vue";
+import Tabs from "../lib/Tabs.vue";
+export default {
+  name: "TabsDemo",
+  components: {
+    Tab,
+    Tabs
+  },
+  setup() {
+    const x = ref("导航1");
+    return {
+      x
+    };
+  }
+};
+</script>
