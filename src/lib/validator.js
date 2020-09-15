5ef14df80d40747d467b12398567414b5fa007c7
GarenWang <2270099149@qq.com>
2020-09-16 07:53:08 +0800

    validator第四版
     validator第四版

--- src/lib/validator.js
+++ src/lib/validator.js
@@ -1,66 +1,70 @@
+export default class Validator {
+  constructor() {
 
-export default function validate(data, rules) {
-  let errors = {}
-  rules.forEach((rule) => {
-    let value = data[rule.key]
-    if (rule.required) {
-      let error = validate.required(value)
-      if (error) {
-        ensureObject(errors, rule.key)
-        errors[rule.key].required = error;
-        return
-      }
-    }
+  }
 
-    // 遍历validator
-    let validators = Object.keys(rule).filter(key => key !== 'key' || key !== 'required')
-    validators.forEach((validatorKey) => {
-      // rule.key
-      if (validate[validatorKey]) {
-        let error = validate[validatorKey] && validate[validatorKey](value, rule[validatorKey])
+  static add(name, fn) {
+    Validator.prototype[name] = fn;
+  }
+
+  validate(data, rules) {
+    let errors = {}
+    rules.forEach((rule) => {
+      let value = data[rule.key]
+      if (rule.required) {
+        let error = this.required(value)
         if (error) {
           ensureObject(errors, rule.key)
-          errors[rule.key][validatorKey] = error;
+          errors[rule.key].required = error;
+          return
         }
-      } else {
-        throw 'invalid validator rule: ' + validatorKey
       }
 
+      // 遍历validator
+      let validators = Object.keys(rule).filter(key => key !== 'key' || key !== 'required')
+      validators.forEach((validatorKey) => {
+        // rule.key
+        if (this[validatorKey]) {
+          let error = this[validatorKey](value, rule[validatorKey])
+          if (error) {
+            ensureObject(errors, rule.key)
+            errors[rule.key][validatorKey] = error;
+          }
+        } else {
+          throw 'invalid validator rule: ' + validatorKey
+        }
+
+      })
+
     })
+  }
 
+  required(value) {
+    if (value !== 0 && !value) {
+      return '必填'
+    }
+  }
 
-  })
-}
+  pattern(value, pattern) {
+    if (pattern === 'email') {
+      pattern = /^.+@.+$/
+    }
+    if (pattern.test(value) === false) {
+      return '格式不正确'
+    }
+  }
+  minLength(value, minLength) {
+    if (value.length < minLength) {
+      return '太短'
+    }
+  }
 
-validate.required = (value) => {
-  if (value !== 0 && !value) {
-    return '必填'
+  maxLength(value, maxLength) {
+    if (value.length > minLength) {
+      return '太长'
+    }
   }
 }
 
-validate.pattern = (value, pattern) => {
-  if (pattern === 'email') {
-    pattern = /^.+@.+$/
-  }
-  if (pattern.test(value) === false) {
-    return '格式不正确'
-  }
-}
-validate.minLength = (value, minLength) => {
-  if (value.length < minLength) {
-    return '太短'
-  }
-}
-
-validate.maxLength = (value, maxLength) => {
-  if (value.length > minLength) {
-    return '太长'
-  }
-}
 
 
-function ensureObject(obj, key) {
-  if (typeof obj[key] !== 'object') {
-    obj[key] = {}
-  }
-}
